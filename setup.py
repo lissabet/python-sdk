991c2f244b55fd0702fa64946f4207b85f33915b
yuvalb <yuval@alooma.io>
2019-03-08 18:37:13 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-try:
-    from pip.req import parse_requirements
-except ImportError:
-    from pip._internal.req import parse_requirements
-
-install_reqs = parse_requirements("requirements.txt")
-test_reqs = parse_requirements("requirements-tests.txt")
-reqs = [str(ir.req) for ir in install_reqs]
 sdk_package_name = 'alooma_pysdk'
 packages = [sdk_package_name]
 
@@ -17,14 +9,13 @@ setup(
     name=sdk_package_name,
     packages=packages,
     package_data={sdk_package_name: ['alooma_ca']},
-    version='2.0.2',
+    version='3.0.0',
     description='An easy-to-integrate SDK for your Python apps to report '
                 'events to Alooma',
     url='https://github.com/Aloomaio/python-sdk',
     author='Alooma',
     author_email='integrations@alooma.com',
     keywords=['python', 'sdk', 'alooma', 'pysdk'],
-    install_requires=install_reqs,
-    tests_require=test_reqs
-
+    install_requires=open("requirements.txt").readlines(),
+    tests_require=open("requirements-tests.txt").readlines()
 )
